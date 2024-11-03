0406d1cb00a390297ab8b81ebfb944c36592ff65
theAydogan <osmanahmet13@gmail.com>
2024-11-03 12:09:00 -0500

    Search engine page added

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,6 +1,27 @@
-<!-- resources/views/documents/search.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search Documents</title>
+</head>
+<body>
+    <h1>Search Documents</h1>
 
-<form action="{{ route('documents.search') }}" method="GET">
-    <input type="text" name="query" placeholder="Search documents..." required>
-    <button type="submit">Search</button>
-</form>
+    <form action="{{ route('documents.search') }}" method="GET">
+        <input type="text" name="query" placeholder="Search documents..." required>
+        <button type="submit">Search</button>
+    </form>
+
+    @if(isset($documents))
+        <h2>Results:</h2>
+        @if($documents->isEmpty())
+            <p>No documents found.</p>
+        @else
+            <ul>
+                @foreach($documents as $document)
+                    <li>{{ $document->title }}</li>
+                @endforeach
+            </ul>
+        @endif
+    @endif
+</body>
+</html>
